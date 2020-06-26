8ef85380be75999e6400c5a76fb87a09161b4706
Camila <camila.bodack@gmail.com>
2020-06-26 18:42:32 -0300

    optimize function

--- src/palindrome_pattern.py
+++ src/palindrome_pattern.py
@@ -2,12 +2,10 @@
 
 
 def is_palindrome(word):
+
     '''
         Function to verify palindrome pattern in string
         :param word: string to be verified
     '''
 
-    if word == word[::-1]:
-        return True
-    else:
-        return False
+    return word == word[::-1]
